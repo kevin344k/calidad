51a02bd0909fe99e921cbaf752362cef6a510921
kevin344k <kevin344k@gmail.com>
2025-08-29 16:51:33 -0500

    ws

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,28 +5,43 @@ export default function Home() {
   const navigate = useNavigate();
 
   return (
-    <div className=" flex  gap-4 justify-center items-center bg-gradient-to-tr from-indigo-600  to-purple-500 min-h-screen">
-    <div className="flex flex-col w-[80%] text-center gap-4 justify-center items-center">
+    <div className=" flex  gap-4 justify-center items-center ">
+      <div className="flex flex-col w-[80%] text-center gap-4 justify-center items-center">
         <div
-        className="p-5 w-full bg-blue-500 text-white rounded-lg cursor-pointer"
-        onClick={() => navigate("/suministros")}
-      >
-      Suministros
-      </div>
-      <div
-        className="p-5 w-full bg-green-500 text-white rounded-lg cursor-pointer"
-        onClick={() => navigate("/ft-productos")}
-      >
-        Productos
-      </div>
+          className="p-5 w-[270px] h-[270px] rounded-lg cursor-pointer"
+          onClick={() => navigate("/suministros")}
+        >
+  
+         <div className="w-full h-full rounded-tl-lg rounded-tr-lg overflow-hidden object-cover">
+                    <img className="w-full h-full" src="https://www.earturo.com.do/web/image/product.template/1014/image" alt="" />
+          </div>
+        <div className="bg-green-500 text-white rounded-bl-lg rounded-br-lg py-3">
+          <p>  Suministros</p>
+        </div>
+        </div>
         <div
-        className="p-5 w-full bg-green-500 text-white rounded-lg cursor-pointer"
-        onClick={() => navigate("/resinas")}
-      >
-        Resinas
+          className="p-5 w-[270px] h-[270px] rounded-lg cursor-pointer"
+          onClick={() => navigate("/ft-productos")}
+        >
+            <div className="w-full h-full rounded-tl-lg rounded-tr-lg overflow-hidden object-cover">
+                    <img className="w-full h-full" src="https://zodiplast.com.ec/wp-content/uploads/2022/08/SUPER-VIANDA-AMARILLA-DIPOR-X-25-UND-SVA-2.jpg" alt="" />
+          </div>
+        <div className="bg-green-500 text-white rounded-bl-lg rounded-br-lg py-3">
+          <p>  Productos</p>
+        </div>
+        </div>
+        <div
+          className="p-5 w-[270px] h-[270px] cursor-pointer"
+          onClick={() => navigate("/resinas")}
+        >
+          <div className="w-full h-full rounded-tl-lg rounded-tr-lg overflow-hidden object-cover">
+                    <img className="w-full h-full" src="https://d2n4wb9orp1vta.cloudfront.net/cms/brand/PT-Mex/2019-PT-Mex/bamberger.png;maxWidth=1200" alt="" />
+          </div>
+        <div className="bg-green-500 text-white rounded-bl-lg rounded-br-lg py-3">
+          <p>  Resinas</p>
+        </div>
+        </div>
       </div>
-    </div>
-
     </div>
   );
 }
